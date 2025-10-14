7a4408bd7dbf8970ebeb401ab8c965e365ac7e8d
prateeksha-coder <prateeksha.codingal@gmail.com>
2025-10-14 20:00:10 +0530

    commit

--- i.py
+++ i.py
@@ -4,6 +4,7 @@ current_time = datetime.datetime.now()
  
 print("Time now at greenwich meridian is : ", end = "")   
 print(current_time)
+#print(current_time.strftime("%A"))
 
 import calendar
 print("\n", calendar.calendar(2025))
