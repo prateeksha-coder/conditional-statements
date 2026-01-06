8d867734a1d3746deb5c23cbfa40bc7fce4acc1e
prateeksha-coder <prateeksha.codingal@gmail.com>
2026-01-06 14:48:53 +0530

    commit

--- ride.py
+++ ride.py
@@ -1,33 +1,31 @@
 print("Select your ride: ")
 print("1. Bike")
 print("2. Car")
-#take input of number 1 or 2
-#select your ride
+
 choice = int( input("Enter your choice: ") )
-#User entering option 1 
-if( choice == 1 ): #condition 1 outer if statement
+
+if( choice == 1 ):
   print( "what type of bike? " )
   print("1.Scooty\n")
   print("2.Scooter\n")
 
-  #Condition for selecting the type of bike
+  
   choice2=int(input("Enter you choice2: "))
   if choice2==1: #inner if statement
     print("you have selected scooty")
   else:
     print("you have selected scooter")
 
-#User entering option 2
-elif( choice == 2 ): #outer elif statement
+
+elif( choice == 2 ): 
   print( "what type of car?" )
   print("1.Sedan")
   print("2.XUV")
   choice3=int(input("enter your choice3: "))
-  if choice3==1: #inner if statement
-  #condition for selecting the type of car
+  if choice3==1: 
     print("you have selected sedan")
   else:
     print("you have selected XUV")
 
-else: #outer else statement
+else: 
   print("Wrong choice!")
